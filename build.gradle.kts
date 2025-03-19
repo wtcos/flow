11edc83cb99640d89ff5a1feef2b539b0cb601b6
GeePaw Hill <geepawhill@geepawhill.org>
2025-03-19 13:42:43 -0400

    Version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ deployer {
     // 2. Project details.
     // https://opensource.deepmedia.io/deployer/configuration
     projectInfo {
-        version = "0.0.2"
+        version = "1.0.0"
         description.set("A sample project to showcase Maven Central publications.")
         url.set("https://github.com/wethinkcode/flow")
         scm.fromGithub("wethinkcode", "flow")
